40a276c29adf5689a5ec32d3c73d1e4b12982c99
Patrick.C.Campbell@noaa.gov <Patrick.C.Campbell@m71a1.ncep.noaa.gov>
2021-05-19 21:00:12 +0000

    Updated small issue with write statement.

--- parallel/src/rdfv3.f90
+++ parallel/src/rdfv3.f90
@@ -2037,7 +2037,7 @@ SUBROUTINE rdfv3 (mcip_now,nn)
            call myinterp(dum3d_l(:,:,k),met_nx,met_ny,atmp,xindex,yindex,ncols_x,nrows_x,1)
            lufrac(1:ncols_x,1:nrows_x,k) = atmp(1:ncols_x,1:nrows_x)
          enddo
-          WRITE (*,ifmt2) 'LANDUSEF ', lufrac(lprt_metx,lprt_mety,:)
+          WRITE (*,ifmt2) 'LANDUSEF ', (lufrac(lprt_metx,lprt_mety,k),k=1,nummetlu)
         ELSE
           WRITE (*,f9400) TRIM(pname), 'LANDUSEF', TRIM(nf90_strerror(rcode))
           CALL graceful_stop (pname)
