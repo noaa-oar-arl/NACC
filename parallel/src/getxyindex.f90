d3f8c0fa02ba9b9ea4589cc740e969652511b2cc
youhua tang <youhua.tang@dlogin04.dogwood.wcoss2.ncep.noaa.gov>
2022-12-13 21:30:48 +0000

    change interp routines for polar projection

--- parallel/src/getxyindex.f90
+++ parallel/src/getxyindex.f90
@@ -48,19 +48,45 @@ SUBROUTINE getxyindex (xlat,xlon,xi,yj,tlat,tlon,ix,jy)
  xlontmp=xlon
 
  if(xlontmp.lt.0) xlontmp=xlontmp+360
-  do i=1,ix
+ if(xlontmp.gt.tlon(ix).and.xlontmp.le.tlon(1)+360) then
+  xi=ix+(xlontmp-tlon(ix))/(tlon(1)-tlon(ix)+360)
+ else if (xlontmp.ge.tlon(ix)) then 
+  do i=1,ix-1
    if(xlontmp.ge.tlon(i).and.xlontmp.le.tlon(i+1)) then
     xi=i+(xlontmp-tlon(i))/(tlon(i+1)-tlon(i))
     exit
    endif
   enddo
+ else
+  print*,'wrong xlontmp ',xlontmp,tlon(ix),tlon(1)
+ endif
 
-  do j=1,jy
+  yj=0.
+  do j=1,jy-1
    if(xlat.le.amax1(tlat(j),tlat(j+1)).and.xlat.ge.amin1(tlat(j),tlat(j+1))) then
 ! fv3 is from north to south
     yj=j+(xlat-tlat(j))/(tlat(j+1)-tlat(j))
     exit
    endif
   enddo
+  if(yj.le.1e-32) then
+     if(tlat(1).gt.tlat(2)) then ! north to south
+        if(xlat.ge.tlat(1)) then
+          yj=1.
+        else if (xlat.le.tlat(jy)) then
+          yj=jy
+        endif
+      else  ! south to north
+        if(xlat.le.tlat(1)) then
+           yj=1.
+        else if (xlat.ge.tlat(jy)) then
+           yj=jy
+        endif
+      endif
+   if(yj.le.1e-32) then
+      print*,'wrong xlat ',xlat
+      stop
+   endif
+ endif
  
 END SUBROUTINE getxyindex
